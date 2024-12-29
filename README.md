30fe1984b74d36aa8c84992fbc943cbd9e4faf4a
Nicolas Child <nick@zenpayments.com>
2024-12-29 12:45:47 -0700

    Defined tokens, token types
    Onto the lexical analysis

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # monkey-interpreter
+
 My personal study from Writing an Interpreter in Go by Thorsten Ball
+
+## Lexing
---
+++ token/token.go
@@ -0,0 +1,37 @@
+package token
+
+
+// Our token types
+type TokenType string
+
+type Token struct {
+  Type TokenType
+  Literal string
+}
+
+// Possible tokens
+const (
+  ILLEGAL = "ILLEGAL"
+  EOF = "EOF"
+
+  // Identifiers + literals
+  IDENT = "IDENT"
+  INT = "INT"
+
+  // Operators
+  ASSIGN = "="
+  PLUS = "+"
+
+  //Delimiters
+  COMMA = ","
+  SEMICOLON = ";"
+
+  LPAREN = "("
+  RPAREN = ")"
+  LBRACE = "{"
+  RBRACE = "}"
+
+  //Keywords
+  FUNCTION = "FUNCTION"
+  LET = "LET"
+)
