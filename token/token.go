86ceb861e96481383be45e89ea3d489b54199fe0
Nicolas Child <nick@zenpayments.com>
2024-12-29 13:56:15 -0700

    Token helpers for identifiers

--- token/token.go
+++ token/token.go
@@ -34,3 +34,18 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+// Takes an identifier and returns its token
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	// If we match our map for our special identifiers, return
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	// Otherwise, return it as a regular identifier (variable, function names)
+	return IDENT
+}
